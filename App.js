a028b57cf2d169789ae72fefa36360b28955226a
Jason Lim <jason_lim@rp.edu.sg>
2024-06-02 22:10:08 +0800

    implemented RESTful API

--- App.js
+++ App.js
@@ -17,6 +17,94 @@ let books = [
 ];
 
 
+//RESTful API
+//-------------------------------------------------------------------
+
+
+//GET /books
+app.get('/books', function(req, res) {
+    res.json(books);
+});
+
+//GET /books/1
+app.get('/books/:id', function(req, res) {
+    const id = req.params.id;
+    let book = books.find(b => b.id == id);
+    if (!book) {
+        res.json({ error: 'Book not found' });
+        return;
+    }
+    res.json(book);
+});
+
+//POST /books
+app.post('/books', function(req, res) {
+    const title = req.body.title;
+    const author = req.body.author;
+    const year = req.body.year;
+
+    //generate a new id
+    const id = books[books.length-1].id + 1;
+
+    //create a new book object
+    const book = { id, title, author, year };
+
+    //add the book to the array
+    books.push(book);
+
+    //return the newly created book
+    res.json(book);
+});
+
+//PUT /books/1
+app.put('/books/:id', function(req, res) {
+    const id = req.params.id;
+    const title = req.body.title;
+    const author = req.body.author;
+    const year = req.body.year;
+
+    let book = books.find(b => b.id == id);
+    if (!book) {
+        res.json({ error: 'Book not found' });
+        return;
+    }
+
+    //update the book in the array
+    book.title = title;
+    book.author = author;
+    book.year = year;
+
+    //return the newly updated book
+    res.json(book);
+});
+
+
+//DELETE /books/1
+app.delete('/books/:id', function(req, res) {
+    const id = req.params.id;
+    let book = books.find(b => b.id == id);
+    if (!book) {
+        res.json({ error: 'Book not found' });
+        return;
+    }
+
+    //option 1 - filter
+    books = books.filter(b => b.id != id);
+
+
+    //option 2 - loops
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].id == id) {
+            books.remove(i);
+            break;
+        }
+    }
+
+    //return the deleted book
+    res.json(book);
+});
+
+
 
 
 //-------------------------------------------------------------------
